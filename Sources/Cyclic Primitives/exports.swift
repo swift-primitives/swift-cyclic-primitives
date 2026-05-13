0e99381a86e64e1b77ca9fd250f7d2a6694b8af4
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-05-13 10:22:59 +0200

    chore: replace autogen exports.swift header with Apache-2.0 banner

--- Sources/Cyclic Primitives/exports.swift
+++ Sources/Cyclic Primitives/exports.swift
@@ -1,5 +1,13 @@
-// exports.swift
-// Re-export dependencies for consumers.
+// ===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-primitives open source project
+//
+// Copyright (c) 2024-2026 Coen ten Thije Boonkkamp and the swift-primitives project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE for license information
+//
+// ===----------------------------------------------------------------------===//
 
 @_exported public import Cardinal_Primitives
 @_exported public import Comparison_Primitives
